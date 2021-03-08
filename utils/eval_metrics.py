98b0bc89a11770ba723e180b0bd31e1ab07d2dd0
nik1806 <nikhil.paliwal14@gmail.com>
2021-03-08 12:35:50 +0100

    Bug fix: roc_auc score

--- utils/eval_metrics.py
+++ utils/eval_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
-def dice_coefficient(ground_truth, prediction, n_classes:int =21, smooth:float = 0.0001):
+def dice_coefficient_custom(ground_truth, prediction, n_classes:int =21, smooth:float = 0.0001):
     """
         Return dice coefficient score.
     Args:   
@@ -31,3 +32,38 @@ def dice_coefficient(ground_truth, prediction, n_classes:int =21, smooth:float =
     # return
     return score/n_classes
     
+
+def roc_auc_custom(ground_truth, prediction, n_classes:int =21, average:str = 'macro'):
+    """
+        Return roc_auc score (average for all class for each image).
+    Args:   
+        ground_truth: HxW ndarray; each element contains class label from (0-20)
+        prediction: HxW ndarray; each element contains predicted class label
+        n_class: total number of classes; used to average score
+        average: type of averaging, passed to sklearn's roc_auc_score() function 
+    """
+    # init, sum over all individual class scores
+    score = 0.
+    cnt = 0
+    # iterate for each class (0-20)
+    for c in range(n_classes):
+        # create 2D matrix (mask) (binary elements 0-1) for ground truth and prediction matrix
+        gnd = np.zeros_like(ground_truth)
+        pred = np.zeros_like(prediction)
+        gnd[ground_truth == c] = 1
+        pred[prediction == c] = 1
+    
+        # compute score
+        gnd_f = gnd.flatten() # flatten to 1D
+        pred_f = pred.flatten()
+        # roc_auc need atleast one element of each class, otherwise the score is not defineds
+        # due to class imbalance it can't be possible always, thus we are avoiding those instances
+        try:
+            score += roc_auc_score(gnd_f, pred_f, average=average) # binary 
+            cnt += 1 # accurate averaging
+        except ValueError: 
+            pass
+
+    # average n=21
+    # return
+    return score/cnt
